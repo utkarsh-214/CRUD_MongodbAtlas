d039f2f5a4c41c660b1e0cbbbe197849ecd59d1d
UTKARSH.s <utkarsh.sharma20@st.niituniversity.in>
2023-09-14 00:19:43 +0530

    all the crud operations added and .env file not included

--- app.js
+++ app.js
@@ -1,5 +1,61 @@
 const express = require("express");
 const app = express();
+const db = require("./connection");
+const schema = require("./schema");
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//crud applications
+app.post('/', async(req, res)=>{
+    const {title, content} = req.body;
+    try{
+        const newPost = await schema.create({title, content});
+        res.json(newPost);
+    }catch(err){
+        res.status(500).send(error);
+    }
+});
+
+app.get('/', async(req, res)=>{
+    try {
+        const posts = await schema.find();
+        res.json(posts);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get('/:id', async(req, res)=>{
+    const {id} = req.params;
+    try{
+        const post = await schema.findById(id);
+        res.json(post);
+    }catch(error){
+        res.status(500).send(error);
+    }
+});
+
+app.put('/:id', async(req, res)=>{
+    const {id} = req.params;
+    const {title, content} = req.body;
+    try{
+        const post = await schema.findByIdAndUpdate(id, {title, content});
+        res.json('Update Successful');
+    }catch(error){
+        res.status(500).send(error);
+    }
+});
+
+app.delete('/:id', async(req, res)=>{
+    const {id} = req.params;
+    const {title, content} = req.body;
+    try{
+        const post = await schema.findByIdAndDelete(id, {title, content});
+        res.json('Delete Successful');
+    }catch(error){
+        res.status(500).send(error);
+    }
+});
 
 app.listen(3000, () => {
     console.log("listening to port 3000");
